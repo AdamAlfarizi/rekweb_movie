7bd0eebf5ce744a69ac894f4ce25ef7a9f7a1d61
adam <adamalfal8@mail.unpas.ac.id>
2019-12-19 17:49:04 +0700

    perbaikan japa tapi masish eror

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
+function getMovie(keyword){
+    let xhr = new XMLHttpRequest();
 
-
-function getMovies(keyword){
-	let xhr = new XMLHttpRequest();
-
-	xhr.onreadytatechange = function(){
-		if (xhr.readyState === 4 && xhr.status ===200) {
-			let movies = JSON.perse(xhr.response);
-			showMovies(movies.Search);		}
-	}
-	xhr.open('get','http://www.omdbapi.com/?apikey=e09c00d4&s='+ keyword);
-	xhr.send();
-}
-function showMovies(movies){
-	let cards = "";
-	movies.forEach(function(movie){
-		cards += `<div class="col-4 my-3">
-              <div class="card">
-                <img src="${movie.Poster}" class="card-img-top">
-                <div class="card-body">
-                  <h5 class="card-title"> ${movie.Title} </h5>
-                  <h5 class="card-subtitle mb-2 text-muted">${movie.Year}</h5>
-                  <a href="detail.php?id=${movie.imdbID}" class="btn btn-primary"> Show detail</a>
-                </div>
-            </div>
-          </div>`;
-	});
-	movieList.innerHTML = cards;
+    xhr.onreadystatechange = function(){
+        if( xhr.readyState === 4 && xhr.status === 200 ){
+            //ketika ajax siap
+            let movies = JSON.parse(xhr.response);
+            showMivies(movies.Search);
+        }
+    }
+    xhr.open('get', 'http://www.omdbapi.com/?apikey=e09c00d4&s=' +keyword);
+    xhr.send();
 }
 
+//menampilkan film
+function showMivies(movies){
+    if (!movies) {
+        movieList.innerHTML = '<p class="alert alert-danger">not found</p>';
+        return;
+    }
+
+    let cards = '';
+    movies.forEach(function(movie){
+        cards += `<div class="col-4 my-4">
+                <div class="card">
+                     <img src="${movie.Poster}" class="card-img-top">
+                     <div class="card-body">
+                         <h5 class="card-title">${movie.Title}</h5>
+                         <h6 class="card-subtitle mb-2 text-muted">${movie.Year}</h6>
+                          <a href="detail.php?id=${movie.imdbID}" class="btn btn-primary">Show Detail</a>
+                      </div>
+                 </div>
+                </div>`;
+    });
+    movieList.innerHTML = cards;
+}
 let movieList = document.querySelector('.movie-list');
+let inputKeyword = document.querySelector('.input-keyword');
+let btnKeyword = document.querySelector('.btn-search');
 
-getMovies('naruto');
+
+//ketika halaman dibuka
+getMovie('naruto');
+
+//ketika film dicari
+btnKeyword.addEventListener('click', function(){
+    getMovie(inputKeyword.value);
+});
